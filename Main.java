76553b1062b3fab7ce7737ccd0054d5f77896ca9
lavanderhoney <milapp857@gmail.com>
2024-04-01 00:22:48 +0530

    Init the file

---
+++ Main.java
@@ -0,0 +1,106 @@
+import java.util.ArrayList;
+import java.util.HashMap;
+import java.util.HashSet;
+import java.util.Scanner;
+import java.util.Set;
+import java.util.Arrays;
+
+public class Main {
+
+    public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+        int n = 5;
+
+        HashMap<String, String> dict_fds = new HashMap<>();
+        String rel_schema = "";
+        System.out.println("Enter the relation schema as a string:");
+
+        rel_schema = sc.nextLine();
+
+        for (int i = 0; i < n; i++) {
+            String s = sc.nextLine();
+            String[] fds = s.split(" "); // 0: determinant 1: dependent
+            String temp;
+            if (dict_fds.containsKey(fds[0])) {
+                temp = dict_fds.get(fds[0]);
+                temp += fds[1];
+                dict_fds.put(fds[0], temp);
+            } else {
+                dict_fds.put(fds[0], fds[1]);
+            }
+        }
+        // printLinks(links);
+        ArrayList<String> candidate_keys = getCKeys(dict_fds, rel_schema);
+        System.out.println("Func dependencies: " + dict_fds);
+        System.out.println("Candidate keys: " + candidate_keys);
+        sc.close();
+    }
+
+    public static ArrayList<String> getCKeys(HashMap<String, String> dict_fds, String schema) {
+        ArrayList<String> ck = new ArrayList<>();
+        ArrayList<String> combos = new ArrayList<>();
+        allCombos(schema, 0, combos, "");
+        // System.out.println(combos);
+        for (String attr : combos) {
+            String closure = sortString(getClosureSet(attr, dict_fds));
+            // System.out.println("CLosure of " + attr + ": " + closure);
+            if (closure.equals(schema)) {
+                ck.add(attr);
+            }
+        }
+
+        return ck;
+    }
+
+    public static String getClosureSet(String s, HashMap<String, String> dict_fds) {
+        StringBuilder finds = new StringBuilder();
+        finds.append(s);
+        Set<String> dets = dict_fds.keySet();
+        ArrayList<String> determinants = new ArrayList<>();
+        determinants.addAll(dets);
+        for (int i = 0; i < determinants.size(); i++) {
+            String attr = determinants.get(i);
+            if (searchIn(attr, finds.toString()) && !searchIn(dict_fds.get(attr), finds.toString())) {
+                finds.append(dict_fds.get(attr));
+                determinants.remove(attr);
+                i = -1;
+            }
+        }
+        return finds.toString();
+    }
+
+    public static boolean searchIn(String target, String finds) {
+        boolean main_flag = true;
+        boolean sub_flag = false;
+        for (String s : target.split("")) {
+            sub_flag = false;
+            for (String fs : finds.split("")) {
+                if (s.equals(fs)) {
+                    sub_flag = true;
+                }
+            }
+            if (!sub_flag) {
+                main_flag = false;
+            }
+        }
+        return main_flag;
+    }
+
+    public static String sortString(String inputString) {
+        char tempArray[] = inputString.toCharArray();
+        Arrays.sort(tempArray);
+        return new String(tempArray);
+    }
+
+    public static void allCombos(String schema, int indx, ArrayList<String> res, String temp) {
+        if (indx == schema.length()) {
+            res.add(temp);
+            return;
+        }
+        allCombos(schema, indx + 1, res, temp);
+        temp += schema.charAt(indx);
+        allCombos(schema, indx + 1, res, temp);
+        // return res;
+    }
+
+}
