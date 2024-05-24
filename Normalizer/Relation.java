d5b872f3dc880dd5ce4b8962c5bf3a83feb8637e
lavanderhoney <milapp857@gmail.com>
2024-05-24 19:58:19 +0530

    added static import

--- Normalizer/Relation.java
+++ Normalizer/Relation.java
@@ -2,9 +2,11 @@ package Normalizer;
 
 import java.util.ArrayList;
 import java.util.HashMap;
+import java.util.HashSet;
 import java.util.Map;
 import java.util.Set;
 import java.util.concurrent.ConcurrentHashMap;
+import static Normalizer.Helpers.*;
 
 public class Relation {
     // atrributes of relation class
@@ -17,6 +19,10 @@ public class Relation {
     private boolean is2NF;
     private boolean is3NF;
     private boolean isBCNF;
+
+    private Map<String, HashMap<String, String>> reln_2NF;
+    private Map<String, HashMap<String, String>> reln_3NF;
+    private Map<String, HashMap<String, String>> reln_BCNF;
 
     public Relation() {
         // Throw error
@@ -42,6 +48,14 @@ public class Relation {
 
     public void check_for2NF() {
         is2NF = check2NF(relString, candidate_keys, functional_dependencies);
+    }
+
+    public void convert_2NF() {
+        if (!is2NF) {
+            // throw custom error
+            return;
+        }
+        reln_2NF = 
     }
 
     private ArrayList<String> calc_CK(HashMap<String, String> dict_fds, String schema) {
@@ -180,4 +194,69 @@ public class Relation {
         return true;
     }
 
+    private Map<String, HashMap<String, String>> convert2NF(String schema, ArrayList<String> ckeys,
+            HashMap<String, String> fds) {
+        /*
+         * This function splits the relation to 2NF
+         * schema: The relation schema String, provided by the user
+         * ckeys: List of the candidate keys
+         * fds: functional dependencies
+         */
+        Set<String> dets = fds.keySet();
+
+        Map<String, HashMap<String, String>> rel_2nf = new HashMap<String, HashMap<String, String>>();
+        ArrayList<String> split_lhs = new ArrayList<>();
+        String all_attributes = "";
+        for (String attr : dets) {
+            for (String key : ckeys) {
+                if (Helpers.searchIn(attr, key) && !attr.equals(key) && !Helpers.searchIn(fds.get(attr), key)) {
+                    // a PD is found.
+                    HashMap<String, String> fd_2nf = new HashMap<>();
+                    String temp_ = attr;
+                    split_lhs.add(attr);
+                    temp_ += fds.get(attr);
+                    fd_2nf.put(attr, fds.get(attr));
+                    // adding Y+
+                    String depndt_closure = getClosureSet(schema, fds.get(attr), fds);
+                    fd_2nf.putAll(getClosureSetFD(schema, fds.get(attr), fds));
+
+                    // System.out.println("FDs with Y+ as: " + depndt_closure + " is: " + fd_2nf);
+                    temp_ += depndt_closure;
+                    temp_ = refineFinds(temp_).toString(); // remove duplicating attributes
+                    all_attributes += temp_;
+                    rel_2nf.put(temp_, fd_2nf);
+
+                    break;
+                    // rel_2nf_fd.put(attr, fds.get(attr));
+                }
+            }
+        }
+
+        // Creating the joining relation
+        all_attributes = refineFinds(all_attributes).toString();
+        HashSet<String> tempSet = new HashSet<>();
+        for (String s : split_lhs) {
+            for (String ss : s.split("")) {
+                tempSet.add(ss);
+            }
+        }
+        for (String s : schema.split("")) {
+            if (!all_attributes.contains(s)) {
+                tempSet.add(s);
+            }
+        }
+        String join_reln = "";
+        for (String s : tempSet) {
+            join_reln += s;
+        }
+
+        // combine all CKs
+        // String relckey = "";
+        // for (String ckey : ckeys) {
+        // relckey += ckey;
+        // }
+
+        rel_2nf.put(join_reln, new HashMap<>());
+        return rel_2nf;
+    }
 }
